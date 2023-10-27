b4377f18055e8b03eca464f418b45368d2e51b1d
Charles Iliya Krempeaux <iamreiver@gmail.com>
2023-10-27 01:05:33 -0700

    initial commits

--- encodeuint32.go
+++ encodeuint32.go
@@ -1,10 +1,10 @@
 package hexadeca
 
 func EncodeUint32(value uint32, symbolFunc func(byte)rune) (r7 rune, r6 rune, r5 rune, r4 rune, r3 rune, r2 rune, r1 rune, r0 rune) {
-	r7,  r6  = EncodeByte(byte(0x00000000000000ff & (value >> (8 * 3))), symbolFunc)
-	r5,  r4  = EncodeByte(byte(0x00000000000000ff & (value >> (8 * 2))), symbolFunc)
-	r3,  r2  = EncodeByte(byte(0x00000000000000ff & (value >> (8    ))), symbolFunc)
-	r1,  r0  = EncodeByte(byte(0x00000000000000ff &  value            ), symbolFunc)
+	r7,  r6  = EncodeByte(byte(0x000000ff & (value >> (8 * 3))), symbolFunc)
+	r5,  r4  = EncodeByte(byte(0x000000ff & (value >> (8 * 2))), symbolFunc)
+	r3,  r2  = EncodeByte(byte(0x000000ff & (value >> (8    ))), symbolFunc)
+	r1,  r0  = EncodeByte(byte(0x000000ff &  value            ), symbolFunc)
 
 	return
 }
