8b0bf9bc7a965ff9763c814bd386ac5a496e4044
Charles Iliya Krempeaux <iamreiver@gmail.com>
2023-10-27 22:22:02 -0700

    initial commits

---
+++ encodeuint32.go
@@ -0,0 +1,18 @@
+package hexadeca
+
+import (
+	"sourcecode.social/reiver/go-hexadeca/bytes"
+	"sourcecode.social/reiver/go-hexadeca/runes"
+)
+
+func EncodeUint32UsingLowerCaseSymbols(value uint32) (r7 byte, r6 byte, r5 byte, r4 byte, r3 byte, r2 byte, r1 byte, r0 byte) {
+	return hexadecabytes.EncodeUint32UsingLowerCaseSymbols(value)
+}
+
+func EncodeUint32UsingPersianSymbols(value uint32) (r7 rune, r6 rune, r5 rune, r4 rune, r3 rune, r2 rune, r1 rune, r0 rune) {
+	return hexadecarunes.EncodeUint32UsingPersianSymbols(value)
+}
+
+func EncodeUint32UsingUpperCaseSymbols(value uint32) (r7 byte, r6 byte, r5 byte, r4 byte, r3 byte, r2 byte, r1 byte, r0 byte) {
+	return hexadecabytes.EncodeUint32UsingUpperCaseSymbols(value)
+}
